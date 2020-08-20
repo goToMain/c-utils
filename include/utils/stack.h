7e08208093a6a25469c902a8ecda115aa98faf7c
Siddharth Chandrasekaran <csiddharth@vmware.com>
2020-08-20 20:34:43 +0530

    Add stack and queue wrapper arround list_t

---
+++ include/utils/stack.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 Siddharth Chandrasekaran <siddharth@embedjournal.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _UTILS_STACK_H_
+#define _UTILS_STACK_H_
+
+#include <utils/list.h>
+
+typedef struct {
+	slist_t list;
+} stack_t;
+
+#define stack_node_t snode_t
+
+void stack_init(stack_t *stack);
+void stack_push(stack_t *stack, stack_node_t *node);
+int stack_pop(stack_t *stack, stack_node_t **node);
+int stack_get_top(stack_t *stack, stack_node_t **node);
+
+#endif /* _UTILS_STACK_H_ */
---
+++ src/stack.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Siddharth Chandrasekaran <siddharth@embedjournal.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <utils/stack.h>
+
+void stack_init(stack_t *stack)
+{
+	slist_init(&stack->list);
+}
+
+void stack_push(stack_t *stack, stack_node_t *node)
+{
+	slist_appendleft(&stack->list, node);
+}
+
+int stack_pop(stack_t *stack, stack_node_t **node)
+{
+	return slist_popleft(&stack->list, node);
+}
+
+int stack_get_top(stack_t *stack, stack_node_t **node)
+{
+	if (stack->list.head == NULL)
+		return -1;
+
+	*node = stack->list.head;
+	return 0;
+}
