b35753fbc34cfb18688f7e2456ac8f5c8c597aff
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2021-07-30 22:17:18 +0200

    events: Add a simple pipe based event delivering system
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ include/utils/events.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdbool.h>
+
+#ifndef _UTILS_EVENTS_H_
+#define _UTILS_EVENTS_H_
+
+typedef struct {
+	int rfd;
+	int wfd;
+	bool initialized;
+} event_t;
+
+int event_init(event_t *e, bool active);
+void event_cleanup(event_t *e);
+
+bool event_set(event_t *e);
+bool event_is_set(event_t *e);
+
+#endif /* _UTILS_EVENTS_H_ */
---
+++ src/events.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2021 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <sys/fcntl.h>
+
+#include <utils/fdutils.h>
+#include <utils/events.h>
+
+int event_init(event_t *e, bool active)
+{
+	int fds[2];
+	int ret;
+
+	if (pipe(fds) < 0)
+		return -1;
+
+	ret = fcntl_setfl(fds[0], O_NONBLOCK);
+	if (ret < 0)
+		goto error;
+
+	ret = fcntl_setfl(fds[1], O_NONBLOCK);
+	if (ret < 0)
+		goto error;
+
+	e->rfd = fds[0];
+	e->wfd = fds[1];
+	e->initialized = true;
+
+	if (active)
+		event_set(e);
+
+	return 0;
+error:
+	close(fds[0]);
+	close(fds[1]);
+	return -1;
+}
+
+void event_cleanup(event_t *e)
+{
+	if (!e->initialized)
+		return;
+
+	close(e->rfd);
+	close(e->wfd);
+	e->wfd = e->rfd = -1;
+	e->initialized = false;
+}
+
+bool event_set(event_t *e)
+{
+	static const uint64_t value = 1;
+	ssize_t ret;
+
+	if (!e->initialized)
+		return false;
+
+	ret = write_loop(e->wfd, &value, sizeof(value));
+	if (ret < 0)
+		return false;
+
+	return true;
+}
+
+bool event_is_set(event_t *e)
+{
+	if (!e->initialized)
+		return 0;
+
+	return flush_fd(e->rfd);
+}
