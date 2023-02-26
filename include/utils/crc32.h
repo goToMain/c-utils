a01949911a8e631e8c04797c2b2a242532ee9297
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2023-02-26 18:55:41 +0100

    Import a crc32 implemetnation
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ include/utils/crc32.h
@@ -0,0 +1,9 @@
+#ifndef _UTILS_CRC32_H_
+#define _UTILS_CRC32_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+uint32_t compute_crc32(uint32_t seed, const uint8_t *buffer, size_t length)
+
+#endif /* _UTILS_CRC32_H_ */
