82a90acb4a214b62ad39e17bde892e9b0bcbb94b
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2023-02-26 01:53:35 +0100

    random: Add get_random_bytes method
    Add a portable version of get_random_bytes(). It is an adapted version
    from wirguard.
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ include/utils/random.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020-2021 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _UTILS_RANDOM_H_
+#define _UTILS_RANDOM_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/**
+ * @brief Get the secure random bytes from the systems
+ *
+ * @param out Buffer to fill random bytes
+ * @param len Number of random bytes to be filled to `out`
+ * @return true if successful, false otherwise.
+ */
+bool get_random_bytes(uint8_t *out, size_t len);
+
+#endif
---
+++ src/random.c
@@ -0,0 +1,64 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <utils/random.h>
+
+#ifdef __linux__
+#include <sys/syscall.h>
+#endif
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#ifndef MAC_OS_X_VERSION_10_12
+#define MAC_OS_X_VERSION_10_12 101200
+#endif
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12
+#include <sys/random.h>
+#endif
+#endif
+
+#ifndef _WIN32
+bool __attribute__((__warn_unused_result__)) get_random_bytes(uint8_t *out, size_t len)
+{
+	ssize_t ret = 0;
+	size_t i;
+	int fd;
+
+	if (len > 256) {
+		errno = EOVERFLOW;
+		return false;
+	}
+
+#if defined(__OpenBSD__) || \
+	(defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12) || \
+	(defined(__GLIBC__) && (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 25)))
+	if (!getentropy(out, len))
+		return true;
+#endif
+
+#if defined(__NR_getrandom) && defined(__linux__)
+	if (syscall(__NR_getrandom, out, len, 0) == (ssize_t)len)
+		return true;
+#endif
+
+	fd = open("/dev/urandom", O_RDONLY);
+	if (fd < 0)
+		return false;
+	for (errno = 0, i = 0; i < len; i += ret, ret = 0) {
+		ret = read(fd, out + i, len - i);
+		if (ret <= 0) {
+			ret = errno ? -errno : -EIO;
+			break;
+		}
+	}
+	close(fd);
+	errno = -ret;
+	return i == len;
+}
+#else
+#include <ntsecapi.h>
+bool __attribute__((__warn_unused_result__)) get_random_bytes(uint8_t *out, size_t len)
+{
+        return RtlGenRandom(out, len);
+}
+#endif
