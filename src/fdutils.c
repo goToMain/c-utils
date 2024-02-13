65c4c01dd40bb4c7b313bd80dee3b7cc2d1eb7c4
Siddharth Chandrasekaran <siddharth.chandrasekaran@huawei.com>
2024-02-13 17:36:48 +0100

    chore: Update copyright year to 2024
    Signed-off-by: Siddharth Chandrasekaran <siddharth.chandrasekaran@huawei.com>

--- src/fdutils.c
+++ src/fdutils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+ * Copyright (c) 2021-2024 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
