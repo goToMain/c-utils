6a78915f6ac8d46d9e6d188502f989ac745a6a61
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2025-06-16 09:42:40 +0200

    crc: Add support for crc16
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- include/utils/crc.h
+++ include/utils/crc.h
@@ -8,6 +8,16 @@
 extern "C" {
 #endif
 
+#ifdef __ZEPHYR__
+
+#include <zephyr/sys/crc.h>
+
+#else
+
+uint16_t crc16_itu_t(uint16_t seed, const uint8_t *src, size_t len);
+
+#endif
+
 uint32_t compute_crc32(uint32_t seed, const uint8_t *buffer, size_t length);
 
 #ifdef __cplusplus
