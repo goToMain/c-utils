d832ba52a9c610f7b2f2c932e2d9114a17cf99d2
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2026-03-19 11:00:51 +0100

    slab: Stop packing slab.c
    It caused unaligned pointers into struct osdp_pd and rust does not like
    unaligned poiters. It also hurts perforamnce in those systems.
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- src/slab.c
+++ src/slab.c
@@ -10,11 +10,11 @@
 
 #include <utils/slab.h>
 
-PACK(struct slab_unit {
+struct slab_unit {
 	uint32_t leased;
 	uint32_t canary;
 	uint8_t data[];
-});
+};
 
 int slab_init(slab_t *slab, size_t slab_size,
 	      uint8_t *blob, size_t blob_size)
